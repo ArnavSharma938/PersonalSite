ce9d4f8da300302373f4760026201f7e83bdf642
Arnav Sharma <arnavsharma.0914@gmail.com>
2024-07-22 20:54:30 -0400

    Initial

---
+++ components/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Introduction = () => {
+  return (
+    <div className="content">
+      <h1>Introduction</h1>
+      <p>Hello! I'm Arnav Sharma, a passionate Software Engineer and Computational Biologist.</p>
+    </div>
+  );
+}
+
+export default Introduction;

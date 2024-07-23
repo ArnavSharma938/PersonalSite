7116d770489856fab0a2c071630bc2ab7f3ab4ad
Arnav Sharma <arnavsharma.0914@gmail.com>
2024-07-22 21:08:39 -0400

    more vite

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/App.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Arnav Sharma</h2>
+      <ul>
+        <li><Link to="/">Introduction</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
